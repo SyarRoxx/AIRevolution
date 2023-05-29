da3e0764b4505e385176dd50cbe142dba1bd1107
SyarRoxx <132861708+SyarRoxx@users.noreply.github.com>
2023-05-29 12:09:16 -0400

    character file

---
+++ PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   
+   public float moveSpeed;
+   public float jumpForce;
+   public CharacterController controller;
+
+   private Vector3 moveDirection;
+   public float gravityScale;
+
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+        
+        if(controller.isGrounded)
+        {
+            
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpForce;
+            }
+        }
+
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
